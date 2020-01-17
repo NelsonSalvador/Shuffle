ec4cfb1e3d1cfa1bb216aa7894c00d7e382be8d3
Nelson Salvador <nelsonsalvadorzx@gmail.com>
2020-01-17 13:47:51 -0500

    No ficheiro levels_gameplay pus as cartas a serem baralhadas e a verificação se as cartas selecionadas foram iguais, se sim elemina as cartas

--- Levels_gameplay.py
+++ Levels_gameplay.py
@@ -13,38 +13,41 @@ class Levels_gameplay:
      clicked_i = [False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,
      False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False]
      jogo_i = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18]
+     ver = []
+
      if level == 1:
          while m < 12:
             clicked.append(clicked_i[m])
             jogo.append(jogo_i[m])
             m += 1 
      if level == 2:
-         while m < 12:
+         while m < 16:
             clicked.append(clicked_i[m])
             jogo.append(jogo_i[m])
             m += 1 
      if level == 3:
-         while m < 12:
+         while m < 20:
             clicked.append(clicked_i[m])
             jogo.append(jogo_i[m])
             m += 1 
      if level == 4:
-         while m < 12:
+         while m < 24:
             clicked.append(clicked_i[m])
             jogo.append(jogo_i[m])
             m += 1 
      if level == 5:
-         while m < 12:
+         while m < 30:
             clicked.append(clicked_i[m])
             jogo.append(jogo_i[m])
             m += 1 
      if level == 6:
-         while m < 12:
+         while m < 36:
             clicked.append(clicked_i[m])
             jogo.append(jogo_i[m])
             m += 1 
-     k = 0
-     #pygame.time.wait(200)
+     for i in range(0, m + 1):
+         random.shuffle(jogo)
+     Set_t = set()
      while (True):
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
@@ -65,40 +68,59 @@ class Levels_gameplay:
         if level == 1:
             
             while i < 12:
-                pygame.draw.rect(screen, (150,0,0), (x + 70, y + 60, 80, 120), 0)
-                if x + 70 < x_m < x + 70 + 80 and y + 60 < y_m < y + 60 + 120:
-                    pygame.draw.rect(screen, (255,0,0), (x + 70, y + 60, 80, 120), 3)
-                    if mb[0] == True:
-                       clicked[i] = True
-
-                if (clicked[i]):
-                    if jogo[i] == 1:
-                        pygame.draw.rect(screen, (0,150,0), (x + 70, y + 60, 80, 120), 0)
-                    elif jogo[i] == 2:
-                        pygame.draw.rect(screen, (0,150,150), (x + 70, y + 60, 80, 120), 0)
-                    elif jogo[i] == 3:
-                        pygame.draw.rect(screen, (150,150,150), (x + 70, y + 60, 80, 120), 0)
-                    elif jogo[i] == 4:
-                        pygame.draw.rect(screen, (0,214,150), (x + 70, y + 60, 80, 120), 0)
-                    elif jogo[i] == 5:
-                        pygame.draw.rect(screen, (0,150,214), (x + 70, y + 60, 80, 120), 0)
-                    elif jogo[i] == 6:
-                        pygame.draw.rect(screen, (214,150,150), (x + 70, y + 60, 80, 120), 0)
+                if i not in Set_t:
+                    pygame.draw.rect(screen, (150,0,0), (x + 70, y + 60, 80, 120), 0)
+                    if x + 70 < x_m < x + 70 + 80 and y + 60 < y_m < y + 60 + 120:
+                        pygame.draw.rect(screen, (255,0,0), (x + 70, y + 60, 80, 120), 3)
+                        if mb[0] == True:
+                           clicked[i] = True
+                           pygame.time.wait(50)
+    
+                    if (clicked[i]):
+                        if jogo[i] == 1:
+                            pygame.draw.rect(screen, (0,150,0), (x + 70, y + 60, 80, 120), 0)
+                        elif jogo[i] == 2:
+                            pygame.draw.rect(screen, (0,150,150), (x + 70, y + 60, 80, 120), 0)
+                        elif jogo[i] == 3:
+                            pygame.draw.rect(screen, (150,150,150), (x + 70, y + 60, 80, 120), 0)
+                        elif jogo[i] == 4:
+                            pygame.draw.rect(screen, (0,214,150), (x + 70, y + 60, 80, 120), 0)
+                        elif jogo[i] == 5:
+                            pygame.draw.rect(screen, (0,150,214), (x + 70, y + 60, 80, 120), 0)
+                        elif jogo[i] == 6:
+                            pygame.draw.rect(screen, (214,150,150), (x + 70, y + 60, 80, 120), 0)
                 i += 1
                 if i == 3 or i == 6 or i == 9 or i == 12:
                     x = 420 
                     y += 140
                 else :
                     x += 100
+
             while j < 12:
                 if(clicked[j]):
-                    k += 1
+                    ver.append(jogo[j])
+                    ver.append(j)
+                    j += 1
+                    while j < 12:
+                        if (clicked[j]):
+                            ver.append(jogo[j])
+                            ver.append(j)
+                        j += 1
                 j += 1
-            if k == 2:
+            if len(ver) <= 2:
+        
+                ver.clear() 
+            elif ver[0] != ver[2]:
                 clicked = [False,False,False,False,False,False,False,False,False,False,False,False]
-                pygame.time.wait(200)
-            else :
-                k = 0
+                print(ver)
+                ver.clear()
+            elif ver[0] == ver[2]:
+                Set_t.add(ver[1])
+                Set_t.add(ver[3])
+                clicked = [False,False,False,False,False,False,False,False,False,False,False,False]
+                print(ver)
+                ver.clear()
+            
 
         if level == 2:
             while i < 16:
