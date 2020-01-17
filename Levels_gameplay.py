50266fe087b7c2afa6820cca4b06683aa4b0bf11
Nelson Salvador <nelsonsalvadorzx@gmail.com>
2020-01-17 10:33:31 -0500

    Criei o ficheiro Levels_gameplay, que dentro do nivel selecionado desenha as cartas e o que esta dentro de elas conforme o nivel, é neste ficheiro onde se enconta o jogo em si

---
+++ Levels_gameplay.py
@@ -0,0 +1,178 @@
+import pygame
+import random
+
+
+class Levels_gameplay:
+    def __init__(self, screen, level):
+        self.level = level
+        self.screen = screen
+    def game(self, screen, level):
+     m = 0
+     clicked = []
+     jogo = []
+     clicked_i = [False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,
+     False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False,False]
+     jogo_i = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 18]
+     if level == 1:
+         while m < 12:
+            clicked.append(clicked_i[m])
+            jogo.append(jogo_i[m])
+            m += 1 
+     if level == 2:
+         while m < 12:
+            clicked.append(clicked_i[m])
+            jogo.append(jogo_i[m])
+            m += 1 
+     if level == 3:
+         while m < 12:
+            clicked.append(clicked_i[m])
+            jogo.append(jogo_i[m])
+            m += 1 
+     if level == 4:
+         while m < 12:
+            clicked.append(clicked_i[m])
+            jogo.append(jogo_i[m])
+            m += 1 
+     if level == 5:
+         while m < 12:
+            clicked.append(clicked_i[m])
+            jogo.append(jogo_i[m])
+            m += 1 
+     if level == 6:
+         while m < 12:
+            clicked.append(clicked_i[m])
+            jogo.append(jogo_i[m])
+            m += 1 
+     k = 0
+     #pygame.time.wait(200)
+     while (True):
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT):
+                exit()
+         
+        x_m = pygame.mouse.get_pos()[0]
+        y_m = pygame.mouse.get_pos()[1]
+        mb = pygame.mouse.get_pressed()
+        screen.fill((214,200,170))
+        my_font = pygame.freetype.Font("NotoSans-Regular.ttf", 24)
+
+        i = 0
+        x = 420
+        y = 80
+        j = 0
+        
+
+        if level == 1:
+            
+            while i < 12:
+                pygame.draw.rect(screen, (150,0,0), (x + 70, y + 60, 80, 120), 0)
+                if x + 70 < x_m < x + 70 + 80 and y + 60 < y_m < y + 60 + 120:
+                    pygame.draw.rect(screen, (255,0,0), (x + 70, y + 60, 80, 120), 3)
+                    if mb[0] == True:
+                       clicked[i] = True
+
+                if (clicked[i]):
+                    if jogo[i] == 1:
+                        pygame.draw.rect(screen, (0,150,0), (x + 70, y + 60, 80, 120), 0)
+                    elif jogo[i] == 2:
+                        pygame.draw.rect(screen, (0,150,150), (x + 70, y + 60, 80, 120), 0)
+                    elif jogo[i] == 3:
+                        pygame.draw.rect(screen, (150,150,150), (x + 70, y + 60, 80, 120), 0)
+                    elif jogo[i] == 4:
+                        pygame.draw.rect(screen, (0,214,150), (x + 70, y + 60, 80, 120), 0)
+                    elif jogo[i] == 5:
+                        pygame.draw.rect(screen, (0,150,214), (x + 70, y + 60, 80, 120), 0)
+                    elif jogo[i] == 6:
+                        pygame.draw.rect(screen, (214,150,150), (x + 70, y + 60, 80, 120), 0)
+                i += 1
+                if i == 3 or i == 6 or i == 9 or i == 12:
+                    x = 420 
+                    y += 140
+                else :
+                    x += 100
+            while j < 12:
+                if(clicked[j]):
+                    k += 1
+                j += 1
+            if k == 2:
+                clicked = [False,False,False,False,False,False,False,False,False,False,False,False]
+                pygame.time.wait(200)
+            else :
+                k = 0
+
+        if level == 2:
+            while i < 16:
+                 pygame.draw.rect(screen, (150,0,0), (x + 35, y + 60, 80, 120), 0)
+                 i += 1
+                 if i == 4 or i == 8 or i == 12 or i == 16:
+                     x = 420 
+                     y += 140
+                 else :
+                     x += 100
+            i = 0
+            while i < 16:
+                if x + 35 < x_m < x + 35 + 80 and y + 60 < y_m < y + 60 + 120:
+                    pygame.draw.rect(screen, (0,0,150), (x + 35, y + 60, 80, 120), 0)
+                    if mb[0] == True:
+                        pygame.draw.rect(screen, (0,150,0), (x + 35, y + 60, 80, 120), 0)
+                i += 1
+                if i == 4 or i == 8 or i == 12 or i == 16:
+                    x = 420 
+                    y += 140
+                else :
+                    x += 100
+        if level == 3:
+            while i < 20:
+                 pygame.draw.rect(screen, (150,0,0), (x, y + 60, 80, 120), 0)
+                 i += 1
+                 if i == 5 or i == 10 or i == 15 or i == 20:
+                     x = 420 
+                     y += 140
+                 else :
+                     x += 100
+            while i < 16:
+                if x < x_m < x + 80 and y + 60 < y_m < y + 60 + 120:
+                    pygame.draw.rect(screen, (0,0,150), (x, y + 60, 80, 120), 0)
+                    if mb[0] == True:
+                        pygame.draw.rect(screen, (0,150,0), (x, y + 60, 80, 120), 0)
+                i += 1
+                if i == 5 or i == 10 or i == 15 or i == 20:
+                        x = 420 
+                        y += 140
+                else :
+                        x += 100
+        if level == 4:
+             while i < 24:
+                 pygame.draw.rect(screen, (150,0,0), (x - 35, y + 60, 80, 120), 0)
+                 i += 1
+                 if i == 6 or i == 12 or i == 18 or i == 24:
+                     x = 420 
+                     y += 140
+                 else :
+                     x += 100  
+        if level == 5:
+             while i < 30:
+                 pygame.draw.rect(screen, (150,0,0), (x - 35, y, 80, 120), 0)
+                 i += 1
+                 if i == 6 or i == 12 or i == 18 or i == 24 or i == 30:
+                     x = 420 
+                     y += 140
+                 else :
+                     x += 100 
+        if level == 6:
+             while i < 36:
+                 pygame.draw.rect(screen, (150,0,0), (x, y, 70, 105), 0)
+                 i += 1
+                 if i == 6 or i == 12 or i == 18 or i == 24 or i == 30:
+                     x = 420 
+                     y += 115
+                 else :
+                     x += 80  
+        pygame.draw.rect(screen, (150,0,0), (50, 720, 300, 50), 6)
+        my_font.render_to(screen, (175, 735), "EXIT", (128,53,0))
+
+        if 50 < pygame.mouse.get_pos()[0] < 350 and 720 < pygame.mouse.get_pos()[1] < 770:
+            pygame.draw.rect(screen, (255,0,0), (50, 720, 300, 50), 6)
+            if mb[0] == True:
+                return 0
+        pygame.display.flip()
--- Shuffle.py
+++ Shuffle.py
@@ -1,14 +1,17 @@
 import pygame 
 import pygame.freetype
 
+from Levels_gameplay import Levels_gameplay
+
 
 def Menu_inicial(screen):
-     my_font = pygame.freetype.Font("NotoSans-Regular.ttf", 30)
+     my_font = pygame.freetype.Font("NotoSans-Regular.ttf", 24)
      i = 0
      y = 350
      l1 = 3
      l2 = 4 
      level = 0
+     teste1 = Levels_gameplay(screen, level)
      # Desenha no ecrã os botoẽs das opções 
      while i < 6:
          pygame.draw.rect(screen, (150,0,0), (490, y, 300, 50), 0)
@@ -31,122 +34,43 @@ def Menu_inicial(screen):
          pygame.draw.rect(screen, (255,0,0), (490, 350, 300, 50), 6)
          if mb[0] == True:
              level = 1
-             levels(screen, level)
+             teste1.game(screen, level)
+             main()
 
      elif 490 < x_m < 790 and 410 < y_m < 460:
          pygame.draw.rect(screen, (255,0,0), (490, 410, 300, 50), 6)
          if mb[0] == True:
              level = 2
-             levels(screen, level)
+             teste1.game(screen, level)
     
      elif 490 < x_m < 790 and 470 < y_m < 520:
          pygame.draw.rect(screen, (255,0,0), (490, 470, 300, 50), 6)
          if mb[0] == True:
              level = 3
-             levels(screen, level)
+             teste1.game(screen, level)
 
      elif 490 < x_m < 790 and 530 < y_m < 580:
          pygame.draw.rect(screen, (255,0,0), (490, 530, 300, 50), 6)
          if mb[0] == True:
              level = 4
-             levels(screen, level)
+             teste1.game(screen, level)
     
      elif 490 < x_m < 790 and 590 < y_m < 640:
          pygame.draw.rect(screen, (255,0,0), (490, 590, 300, 50), 6)
          if mb[0] == True:
              level = 5
-             levels(screen, level)
+             teste1.game(screen, level)
     
      elif 490 < x_m < 790 and 650 < y_m < 700:
          pygame.draw.rect(screen, (255,0,0), (490, 650, 300, 50), 6)
          if mb[0] == True:
              level = 6
-             levels(screen, level)
+             teste1.game(screen, level)
     
      elif 50 < x_m < 350 and 720 < y_m < 770:
          pygame.draw.rect(screen, (255,0,0), (50, 720, 300, 50), 6)
          if mb[0] == True:
              exit()
-
-def levels(screen, level):
-    while (True):
-         for event in pygame.event.get():
-            if (event.type == pygame.QUIT):
-                exit()
-         
-         screen.fill((214,200,170))
-         my_font = pygame.freetype.Font("NotoSans-Regular.ttf", 30)
-         i = 0
-         x = 420
-         y = 80
-
-         mb_levels = pygame.mouse.get_pressed()
-         # desenha as cartas conforme os níveis
-         if level == 1:
-             while i < 12:
-                 pygame.draw.rect(screen, (150,0,0), (x + 70, y + 60, 80, 120), 0)
-                 i += 1
-                 if i == 3 or i == 6 or i == 9 or i == 12:
-                     x = 420 
-                     y += 140
-                 else :
-                     x += 100
-         if level == 2:
-             while i < 16:
-                 pygame.draw.rect(screen, (150,0,0), (x + 35, y + 60, 80, 120), 0)
-                 i += 1
-                 if i == 4 or i == 8 or i == 12 or i == 16:
-                     x = 420 
-                     y += 140
-                 else :
-                     x += 100
-         if level == 3:
-             while i < 20:
-                 pygame.draw.rect(screen, (150,0,0), (x, y + 60, 80, 120), 0)
-                 i += 1
-                 if i == 5 or i == 10 or i == 15 or i == 20:
-                     x = 420 
-                     y += 140
-                 else :
-                     x += 100
-         if level == 4:
-             while i < 24:
-                 pygame.draw.rect(screen, (150,0,0), (x - 35, y + 60, 80, 120), 0)
-                 i += 1
-                 if i == 6 or i == 12 or i == 18 or i == 24:
-                     x = 420 
-                     y += 140
-                 else :
-                     x += 100  
-         if level == 5:
-             while i < 30:
-                 pygame.draw.rect(screen, (150,0,0), (x - 35, y, 80, 120), 0)
-                 i += 1
-                 if i == 6 or i == 12 or i == 18 or i == 24 or i == 30:
-                     x = 420 
-                     y += 140
-                 else :
-                     x += 100 
-         if level == 6:
-             while i < 36:
-                 pygame.draw.rect(screen, (150,0,0), (x, y, 70, 105), 0)
-                 i += 1
-                 if i == 6 or i == 12 or i == 18 or i == 24 or i == 30:
-                     x = 420 
-                     y += 115
-                 else :
-                     x += 80                   
-
-
-         pygame.draw.rect(screen, (150,0,0), (50, 720, 300, 50), 6)
-         my_font.render_to(screen, (175, 735), "EXIT", (128,53,0))
-
-         if 50 < pygame.mouse.get_pos()[0] < 350 and 720 < pygame.mouse.get_pos()[1] < 770:
-            pygame.draw.rect(screen, (255,0,0), (50, 720, 300, 50), 6)
-            if mb_levels[0] == True:
-             main()
-
-         pygame.display.flip()
 
 
 def main():
